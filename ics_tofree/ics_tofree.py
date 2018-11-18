c9339c61280ed256718815cb1cd0a5fc78fbfd76
Masami Asakawa <masami.askw@gmail.com>
2018-11-18 16:11:59 +0900

    コメント追加

--- ics_tofree/ics_tofree.py
+++ ics_tofree/ics_tofree.py
@@ -1,15 +1,18 @@
+"""Calendarファイル(.ics)内のイベントをすべて「予定なし」に書き換える."""
 import argparse
 from icalendar import Calendar
 import sys
 
 
 def update_transp(cal, transp):
+    """VEVENTコンポーネントのTRANSPプロパティを指定のものに書き換える."""
     for ev in cal.walk():
         if ev.name == 'VEVENT':
             ev['TRANSP'] = transp
 
 
 def main():
+    """メイン関数."""
     parser = argparse.ArgumentParser()
     parser.add_argument('input', nargs='?',
                         type=argparse.FileType(mode='r'), default=sys.stdin)
